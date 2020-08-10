6fe7e82e29f9930fa20d27dc30ddd1a71b1780b5
Christoffer Öhrström <christoffer.j.ohrstrom@gmail.com>
2020-08-10 16:17:59 +0200

    Added persistence and details page for campgrounds

--- projects/yelp-camp/app.js
+++ projects/yelp-camp/app.js
@@ -1,16 +1,37 @@
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser")
+const express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose")
 
-const mongoose = require("mongoose");
-
-mongoose.connect("mongodb://localhost:27017/yelpCamp",
+mongoose.connect("mongodb://localhost:27017/yelp_camp",
     {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
     .then(() => console.log("Connected to DB!"))
     .catch(error => console.log(error.message))
+
+const campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+})
+
+let Campground = mongoose.model("Campground", campgroundSchema)
+
+// CampGround.create(
+//     {
+//         name: "Creepy Woods", image: "https://images.pexels.com/photos/1840421/pexels-photo-1840421.jpeg?auto=compress&cs=tinysrgb&h=350",
+//         description: "This is a camp in a creepy wood. Very scary!"
+//     },
+//     (err, camp) => {
+//         if (err)
+//             console.log(err)
+//         else {
+//             console.log("new created campground: ")
+//             console.log(camp)
+//         }
+//     })
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.set("view engine", "ejs")
@@ -26,21 +47,43 @@ app.get("/", (req, res) => {
 })
 
 app.get("/campgrounds", (req, res) => {
-    res.render("campgrounds", { campgrounds: campgrounds })
-})
-
-app.post("/campgrounds", (req, res) => {
-    let name = req.body.name;
-    let image = req.body.image;
-    let newCampground = { name: name, image: image }
-    campgrounds.push(newCampground)
-
-    res.redirect("campgrounds")
+    Campground.find({}, (err, allCampgrounds) => {
+        if (err)
+            console.log(err)
+        else
+            res.render("campgrounds", { campgrounds: allCampgrounds })
+    });
 })
 
 app.get("/campgrounds/new", (req, res) => {
     res.render("new")
 })
+
+app.get("/campgrounds/:id", (req, res) => {
+    let campId = req.params.id;
+    console.log(req.params)
+    Campground.findById(campId, (err, camp) => {
+        if (err)
+            console.log(err)
+        else
+            res.render("show", { campground: camp });
+    })
+})
+
+app.post("/campgrounds", (req, res) => {
+    let name = req.body.name;
+    let image = req.body.image;
+    let description = req.body.description
+    let newCampground = { name: name, image: image, description: description };
+
+    Campground.create(newCampground, (err, newlyCreated) => {
+        if (err)
+            console.log(err)
+        else
+            res.redirect("campgrounds")
+    })
+})
+
 
 app.listen(3000, () => {
     console.log("working, on port 3000")
